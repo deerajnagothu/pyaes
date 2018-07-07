0952afcc7863181252859afecf233ea58266c909
Deeraj <deerajnagothu10@gmail.com>
2018-07-07 13:15:47 -0400

    Client Server socket code

---
+++ encrypt_frame_Data.py
@@ -0,0 +1,26 @@
+import pyaes
+
+key = "1234567891234567"
+
+iv = "InitializationVe"
+# aes = pyaes.AESModeOfOperationCBC(key, iv = iv)
+
+ciphertext = ''
+encrypter = pyaes.Encrypter(pyaes.AESModeOfOperationCBC(key, iv))
+for line in file('google.txt'):
+    print(line)
+    ciphertext += encrypter.feed(line)
+
+
+ciphertext += encrypter.feed()
+
+
+decrypter = pyaes.Decrypter(pyaes.AESModeOfOperationCBC(key, iv))
+decrypted = decrypter.feed(ciphertext[:len(ciphertext) / 2])
+decrypted += decrypter.feed(ciphertext[len(ciphertext) / 2:])
+decrypted += decrypter.feed()
+
+
+
+print file('google.txt').read() == decrypted
+
